9afe34a6a893ae2b5c8621705890e25654c0b55b
baobeiintech <thangtran882110@gmail.com>
2026-06-22 22:56:13 +0700

    #main add job-service for JD subsystem 2206

---
+++ resume-search-engine/job-service/src/test/java/com/thangtv/jobservice/job_service/JobServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thangtv.jobservice.job_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
